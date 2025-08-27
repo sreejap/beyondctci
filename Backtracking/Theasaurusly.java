0164eaf0b5d08fd3324ba1a62af10213dc847bfa
sreejap <sreeja605@gmail.com>
2025-08-27 17:34:58 -0400

    Update Theasaurusly.java

--- Backtracking/Theasaurusly.java
+++ Backtracking/Theasaurusly.java
@@ -6,10 +6,10 @@ import java.io.*;
 import java.util.*;
 
 /*
- * To execute Java, please define "static void main" on a class
- * named Solution.
+ * To review - time and space complexity
+ * 
  *
- * If you need more classes, simply define them inline.
+ * 
  */
 
 class Solution {
